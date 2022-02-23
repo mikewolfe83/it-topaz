54cedf2e9fbfa63220f6ff15a10f1aa5a02bfd82
mikewolfe83 <mgray2@ualr.edu>
2022-02-23 02:50:23 +0000

    ajax calc update

---
+++ unit converter ajax/assets/js/script.js
@@ -0,0 +1,8 @@
+function calculate() {
+    "use strict";
+
+    // Get a reference to the form - Use the ID of the form
+    var form = $( "#myform" );
+    
+     // If all of the form elements are valid, the get the form values
+     if (form.valid()) {
